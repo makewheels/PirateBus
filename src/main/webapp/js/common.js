e03c6ffb5cef643a294f4afd62264e629bec4467
spring <spring@qbserver.cn>
2019-03-01 22:43:58 +0800

    delete useless in js file

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,17 +1,6 @@
 var baseUrl = "http://192.168.99.193/bus";
 // var baseUrl = "https://qbserver.cn/bus";
 
-// fundebug.com
-function loadScript(url, apikey) {
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = url;
-	script.setAttribute("apikey", apikey);
-	document.body.appendChild(script);
-}
-loadScript("https://js.fundebug.cn/fundebug.1.5.1.min.js",
-		"402ec2d6773bbfa7d26d10ef41cfdb3d901a10d5f5276aa17812e891a1f7eee5")
-
 // MTA
 var _mtac = {};
 (function() {
